40e74126eb9ebb1f106af5e9b23432c6143d04bf
AlexeyGorbachevskiy <alexeygorbachevskiyy@gmail.com>
2020-08-15 22:12:36 +0300

    Updates

---
+++ precache-manifest.e0150ca4cd23bab1a5dd2e28c5158561.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0eb593fa7bacf678ddb492e240468fd5",
+    "url": "/ToDoList/index.html"
+  },
+  {
+    "revision": "b76c0f7b0854fa706dc6",
+    "url": "/ToDoList/static/css/main.50cf5f0b.chunk.css"
+  },
+  {
+    "revision": "138b174f18d477d35159",
+    "url": "/ToDoList/static/js/2.2f02b887.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/ToDoList/static/js/2.2f02b887.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b76c0f7b0854fa706dc6",
+    "url": "/ToDoList/static/js/main.053e06bd.chunk.js"
+  },
+  {
+    "revision": "836b70caa36cf1f7efa4",
+    "url": "/ToDoList/static/js/runtime-main.4e56d59b.js"
+  }
+]);
